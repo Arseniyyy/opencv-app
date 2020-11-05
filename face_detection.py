523ed0ee2f61892fafbddc59dc194fbf0ad9346e
unknown <arskosh05@mail.ru>
2020-11-05 14:43:53 +0700

    images directory added

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,7 @@
 import cv2
 from image_show import viewImage
 
-image_path = './images/girls.jpeg'
+image_path = str(input('Path: '))
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -9,7 +9,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(
     gray,
     scaleFactor=1.1,
-    minNeighbors=5,
+    minNeighbors=6,
     minSize=(10, 10)
 )
 
@@ -21,3 +21,4 @@ for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
 viewImage(image, faces_detected)
+# ./images/face.jpg
